107e5a9dbd69cf0d5d01f48edcbcce15c6f242d6
dodo7 <dodo7@192.168.219.101>
2023-04-22 19:12:54 +0900

    update_3

--- src/MultiplicationTable_2.java
+++ src/MultiplicationTable_2.java
@@ -4,8 +4,8 @@ public class MultiplicationTable_2 {
 	public static void main (String args[])
 	{
 		//그냥 강의 듣기 전에 써보는 코드
-		//사용자에게 숫자를 입력받아 해당하는 숫자의 구구단을 출력하는 프로그램
 		
+		//사용자에게 숫자를 입력받아 해당하는 숫자의 구구단을 출력하는 프로그램
 		Scanner scan = new Scanner(System.in);
 		System.out.println("숫자를 하나 입력해주세요.");
 		int num;
@@ -20,7 +20,17 @@ public class MultiplicationTable_2 {
 		System.out.printf("%d * 7 = %d\n", num,(num*7));
 		System.out.printf("%d * 8 = %d\n", num,(num*8));
 		System.out.printf("%d * 9 = %d\n", num,(num*9));
-
 		scan.close();
+		
+		//구구단 1~9단까지 모두 구현하는 프로그램
+		System.out.println("1~9단 모두 구현합니다.");
+		int i,n;
+		for(i=1;i<10;i++)
+		{
+			for(n=1;n<10;n++)
+			{
+				System.out.printf("%d*%d=%d\n",i,n,(i*n));
+			}
+		}
 	}
 }
