111f69e99a23a8fdbd2691e59ceb81f7d9c5976f
dodo7 <dodo7@192.168.219.101>
2023-04-23 00:39:02 +0900

    update_7

--- src/MultiplicationTable_1.java
+++ src/MultiplicationTable_1.java
@@ -74,6 +74,27 @@ public class MultiplicationTable_1 {
 //			System.out.println(7*j);
 //		}
 		
+		//8,9단
+		System.out.println("구구단 중 출력할 단은? :");
+		Scanner scanner = new Scanner(System.in);
+		int number = scanner.nextInt();
+		System.out.println("사용자가 입력한 값 : "+ number);
+		
+		if (number < 2)
+		{
+			System.out.println("값을 잘못 입력했습니다.");
+		}
+		else if (number > 9)
+		{
+			System.out.println("값을 잘못 입력했습니다.");
+		}
+		else
+		{
+			for(int i=1;i<10;i++)
+			{
+				System.out.println(number*i);
+			}
+		}
 	}
 
 }
